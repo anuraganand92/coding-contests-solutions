937651c42e3119bca0fd1129b4d3d999741c97e2
Anurag Anand <98257282+anuraganand92@users.noreply.github.com>
2023-02-23 19:30:52 +0530

    Create CHEFCAND.cpp

---
+++ codechef/CHEFCAND.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,n,x;
+
+    cin>>t;
+    while (t--) {
+        cin>>n>>x;
+        if (n>x && (n-x)%4==0)
+            cout<<(n-x)/4<<endl;
+        else if (n>x && (n-x)%4!=0)
+            cout<<((n-x)/4)+1<<endl;
+        else
+            cout<<0<<endl;
+    }
+    return 0; 
+}
